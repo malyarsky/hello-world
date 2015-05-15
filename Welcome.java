54dac39cf0fc19fefe26717c3dbce200c2a8c875
Andrey <malyarsky@yandex.ru>
2015-05-15 14:40:57 +0300

    Changes a line

--- Welcome.java
+++ Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
     public static void main(String[] args) {
         String[] greetings = new String[2];
         greetings[0] = "Welcome to Java";
-        greetings[1] = "Second line to show";
+        greetings[1] = "Second line of the greeting";
 
         for(String g : greetings) {
             System.out.println(g);
