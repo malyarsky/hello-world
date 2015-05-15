d5d0132f9128ae9bbdd3ce033110bbc514cb6d07
Andrey <malyarsky@yandex.ru>
2015-05-15 14:36:06 +0300

    To see how branches work
    Changed some lines of the file

--- Welcome.java
+++ Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
     public static void main(String[] args) {
         String[] greetings = new String[2];
         greetings[0] = "Welcome to Java";
-        greetings[1] = "Second line";
+        greetings[1] = "Second line to show";
 
         for(String g : greetings) {
             System.out.println(g);
