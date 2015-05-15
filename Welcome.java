4e8c20c785381806b22afa002171cc97a76b6154
Andrey <malyarsky@yandex.ru>
2015-05-15 14:30:20 +0300

    Created the Java file
    Trying to commit the whole Java project to the repository

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+/**
+ * Created by amalyarsky on 05.05.15.
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        String[] greetings = new String[2];
+        greetings[0] = "Welcome to Java";
+        greetings[1] = "Second line";
+
+        for(String g : greetings) {
+            System.out.println(g);
+        }
+    }
+}
